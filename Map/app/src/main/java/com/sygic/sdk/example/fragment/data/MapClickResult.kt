eacc6e5df60559aedca7e19b0f5cbcd88b7a9077
Peter Matúška <pmatuska@sygic.com>
2021-11-30 22:42:08 +0100

    Map: Map clicking and adding simple object to map

---
+++ Map/app/src/main/java/com/sygic/sdk/example/fragment/data/MapClickResult.kt
@@ -0,0 +1,5 @@
+package com.sygic.sdk.example.fragment.data
+
+import com.sygic.sdk.position.GeoCoordinates
+
+data class MapClickResult(val title: String, val subtitle: String, val position: GeoCoordinates)
